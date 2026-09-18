0a71c2dc48cc080f60573ddc616d662b8b275258
pdrdantasss-web <pdrdantasss@gmail.com>
2026-09-18 18:49:44 -0300

    Update jwt.py

--- security/jwt.py
+++ security/jwt.py
@@ -1,17 +1,23 @@
+import os
 from datetime import datetime, timedelta, timezone
 
 from jose import jwt
 
 
-SECRET_KEY = "troque-por-uma-chave-secreta-grande"
+# Chave secreta usada para assinar os tokens
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "chave-apenas-para-desenvolvimento"
+)
 
+# Algoritmo utilizado para assinar o JWT
 ALGORITHM = "HS256"
 
+# Tempo de validade do token
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
 def criar_access_token(dados: dict):
-
     dados_token = dados.copy()
 
     expire = datetime.now(timezone.utc) + timedelta(
